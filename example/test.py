7e4ea0909a9d2e61cea8f29c74d7731eb0523a61
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-06-08 12:18:49 +0900

    feat: get access_token :tada:

--- example/test.py
+++ example/test.py
@@ -3,7 +3,7 @@ import githubapp
 
 def main():
 
-  with open('env/rmap_jwt_private.key', 'rb') as f_private:
+  with open('env/private.key', 'rb') as f_private:
     private_key = f_private.read()
   with open('env/app_id.key', 'r') as f_app_id:
     app_id = f_app_id.read()
@@ -11,7 +11,7 @@ def main():
     installation_id = f_installation_id.read()
   client_secret = private_key
   auth = githubapp.Auth(app_id, installation_id, client_secret)
-  access_token = auth.get_access_token()["access_token"]
+  access_token = auth.get_access_token()
   print(access_token)
 
 if __name__ == "__main__":
