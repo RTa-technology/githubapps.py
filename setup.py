5076ecb21376eb1091cd17ea77897470a3eb292e
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-06-09 12:22:32 +0900

    add; PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 
 import re
-
+from codecs import open
+from os import path
 import setuptools
 
 version = ''
@@ -8,9 +9,11 @@ with open('githubapps/__init__.py') as f:
     version = re.search(
         r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
-requirements = []
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
+root_dir = path.abspath(path.dirname(__file__))
+
+def _requirements():
+    return [name.rstrip() for name in open(path.join(root_dir, 'requirements.txt')).readlines()]
+
 
 
 with open('README.md', 'r') as f:
@@ -21,11 +24,12 @@ setuptools.setup(
     version=version,
     license='MIT',
     description='This is a wrapper for the Github Apps API.',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/RTa-technology/githubapps.py',
     author='RTa-technology',
-    packages=setuptools.find_packages(),
-    install_requires=requirements,
+    packages=["githubapps"],
+    install_requires=_requirements(),
     classifiers=[
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
