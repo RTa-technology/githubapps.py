b0ea1a733d91132294e86bf74c2078eaa15ee857
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-06-09 18:47:16 +0900

    add: example

---
+++ example/test.py
@@ -0,0 +1,22 @@
+
+import githubapps
+from github import Github
+
+# using an access token
+
+
+def main():
+    with open('env/private.key', 'rb') as f_private:
+        private_key = f_private.read()
+    with open('env/app_id.key', 'r') as f_app_id:
+        app_id = f_app_id.read()
+    with open('env/installation_id.key', 'r') as f_installation_id:
+        installation_id = f_installation_id.read()
+    client_secret = private_key
+    auth = githubapps.AiohttpAuth(app_id, installation_id, client_secret)
+    access_token = auth.get_access_token()
+
+    g = Github(access_token)
+    print(access_token)
+if __name__ == "__main__":
+    main()
